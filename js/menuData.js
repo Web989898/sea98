bf694dcec01c3a2b4d67458f97d1a38c9620a1d9
Web989898 <159864106+Web989898@users.noreply.github.com>
2025-07-17 16:21:57 +0700

    Add files via upload

---
+++ js/menuData.js
@@ -0,0 +1,156 @@
+function getMenuData() {
+    return {
+        appetizers: [
+            {
+                name: "Truffle Arancini",
+                description: "Crispy risotto balls stuffed with black truffle and parmesan, served with saffron aioli",
+                price: "$18",
+                img: "images/arancini.jpg"
+            },
+            {
+                name: "Seared Scallops",
+                description: "Pan-seared scallops with cauliflower purée and pancetta crisps",
+                price: "$24",
+                img: "images/scallops.jpg"
+            },
+            {
+                name: "Burrata Caprese",
+                description: "Fresh burrata with heirloom tomatoes, basil oil, and aged balsamic",
+                price: "$16",
+                img: "images/burrata.jpg"
+            },
+            {
+                name: "Tuna Tartare",
+                description: "Yellowfin tuna with avocado, sesame, and citrus soy dressing",
+                price: "$22",
+                img: "images/tuna.jpg"
+            },
+            {
+                name: "Foie Gras Terrine",
+                description: "House-made foie gras terrine with fig compote and brioche",
+                price: "$28",
+                img: "images/foie.jpg"
+            },
+            {
+                name: "Oyster Selection",
+                description: "Fresh oysters from the Atlantic with mignonette and cocktail sauce",
+                price: "$3.5/ea",
+                img: "images/oysters.jpg"
+            }
+        ],
+        mains: [
+            {
+                name: "Wagyu Beef Tenderloin",
+                description: "Premium wagyu beef with roasted vegetables and red wine reduction",
+                price: "$68",
+                img: "images/wagyu.jpg"
+            },
+            {
+                name: "Lobster Thermidor",
+                description: "Whole lobster with cognac cream sauce and gruyère cheese",
+                price: "$55",
+                img: "images/lobster.jpg"
+            },
+            {
+                name: "Duck Confit",
+                description: "Slow-cooked duck leg with cherry gastrique and potato gratin",
+                price: "$42",
+                img: "images/duck.jpg"
+            },
+            {
+                name: "Chilean Sea Bass",
+                description: "Pan-seared sea bass with miso glaze and Asian vegetables",
+                price: "$48",
+                img: "images/seabass.jpg"
+            },
+            {
+                name: "Rack of Lamb",
+                description: "Herb-crusted lamb with ratatouille and rosemary jus",
+                price: "$52",
+                img: "images/lamb.jpg"
+            },
+            {
+                name: "Vegetarian Risotto",
+                description: "Wild mushroom risotto with truffle oil and aged parmesan",
+                price: "$32",
+                img: "images/risotto.jpg"
+            }
+        ],
+        desserts: [
+            {
+                name: "Chocolate Soufflé",
+                description: "Warm chocolate soufflé with vanilla ice cream and berry coulis",
+                price: "$14",
+                img: "images/souffle.jpg"
+            },
+            {
+                name: "Crème Brûlée",
+                description: "Classic vanilla custard with caramelized sugar and fresh berries",
+                price: "$12",
+                img: "images/creme_brulee.jpg"
+            },
+            {
+                name: "Tiramisu",
+                description: "Traditional Italian dessert with coffee-soaked ladyfingers and mascarpone",
+                price: "$13",
+                img: "images/tiramisu.jpg"
+            },
+            {
+                name: "Lemon Tart",
+                description: "Tart lemon curd with meringue and shortbread crust",
+                price: "$11",
+                img: "images/lemon_tart.jpg"
+            },
+            {
+                name: "Panna Cotta",
+                description: "Silky vanilla panna cotta with seasonal fruit compote",
+                price: "$10",
+                img: "images/panna_cotta.jpg"
+            },
+            {
+                name: "Cheese Selection",
+                description: "Curated selection of artisanal cheeses with accompaniments",
+                price: "$18",
+                img: "images/cheese.jpg"
+            }
+        ],
+        beverages: [
+            {
+                name: "House Wine Selection",
+                description: "Red, white, and rosé wines carefully selected by our sommelier",
+                price: "$8-15",
+                img: "images/wine.jpg"
+            },
+            {
+                name: "Craft Cocktails",
+                description: "Signature cocktails crafted with premium spirits and fresh ingredients",
+                price: "$12-16",
+                img: "images/cocktails.jpg"
+            },
+            {
+                name: "Artisan Coffee",
+                description: "Freshly roasted coffee beans prepared by our skilled baristas",
+                price: "$4-6",
+                img: "images/coffee.jpg"
+            },
+            {
+                name: "Premium Spirits",
+                description: "Aged whiskeys, cognacs, and premium vodkas",
+                price: "$15-45",
+                img: "images/spirits.jpg"
+            },
+            {
+                name: "Fresh Juices",
+                description: "Freshly squeezed orange, apple, and seasonal fruit juices",
+                price: "$5-7",
+                img: "images/juice.jpg"
+            },
+            {
+                name: "Sparkling Water",
+                description: "Premium sparkling and still water selection",
+                price: "$3-5",
+                img: "images/water.jpg"
+            }
+        ]
+    };
+}
